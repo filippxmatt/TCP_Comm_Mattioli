3c6a5ca0e651a8f52e2a1f96ef48e09319221483
Filippo Mattioli <filippomattioli06@gmail.com>
2025-02-06 10:33:13 +0100

    client multipli

--- TCP_Server/src/tcp_server/TCP_Server.java
+++ TCP_Server/src/tcp_server/TCP_Server.java
@@ -4,6 +4,10 @@
  */
 package tcp_server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 /**
  *
  * @author Filippo Mattioli
@@ -15,12 +19,25 @@ public class TCP_Server {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int i=0;
+        int numero=0;
+
+        System.out.println("Quanti host possono formare la connessione?");
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+
+        try {
+            numero= Integer.parseInt(br.readLine());
+            System.out.println("Numero inserito");
+        } catch (IOException e) {
+            System.err.println("Errore inserimento numero client");
+        }
 
         Server s = new Server(2000);
 
-        while() {
+        while(i<numero) {
             s.attendi();
             s.chiudi();
+            i++;
         }
         s.termina();
     }
